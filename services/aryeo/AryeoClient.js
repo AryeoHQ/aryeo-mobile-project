90abb134aa39515f8823538644c01193d75ee46a
Jarrod Parkes <parkesfjarrod@gmail.com>
2023-01-06 19:30:11 -0600

    chore(main): include bearer in example auth headers

--- services/aryeo/AryeoClient.js
+++ services/aryeo/AryeoClient.js
@@ -8,7 +8,7 @@ export default aryeoClient;
 
 export function authHeaders() {
     return {
-        "Authorization": `INSERT API KEY HERE`,
+        "Authorization": `Bearer [API_KEY]`,
         "Aryeo-Group-Id": `6da2f0f0-2dc5-4b88-b1ad-e2c510729b37`,
     };
 }
